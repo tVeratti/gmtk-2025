38c01a037e27e5f24c57cc0f55959f85b5384a73
Tatiana Veratti <TatianaVeratti@gmail.com>
2025-07-30 14:57:50 -0500

    add initial track and map

---
+++ src/track.gd
@@ -0,0 +1,12 @@
+class_name Track
+extends Node3D
+
+
+const SPEED:float = 0.1
+
+
+@onready var path_follow_3d = %PathFollow3D
+
+
+func _process(delta):
+	path_follow_3d.progress_ratio -= SPEED * delta
